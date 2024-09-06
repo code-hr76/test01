bb2d5cfffd591e1a80ab12688620a84c7d177d42
code-hr76 <code.hr76@gmail.com>
2024-09-06 12:55:59 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from fastapi import FastAPI
 from nicegui import ui
+
+app = FastAPI()
 
 t1=ui.input(label='enter value')
 b1=ui.button('BUTTON', on_click=lambda: lab.set_text( t1.value ) )
 lab=ui.label('Hello NiceGUI!')
 
-ui.run()
+ui.run_with(app, storage_secret='secret')
